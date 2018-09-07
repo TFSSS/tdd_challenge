585780db9a8ea9e320a8ec59f85459921d22cb4b
ehu <fehuehuf@gmail.com>
2018-09-07 18:10:06 +0900

    テストケース追加

--- test/calc_price_test.py
+++ test/calc_price_test.py
@@ -5,8 +5,22 @@ import io
 
 class CalcPriceTest(unittest.TestCase):
     def testCreate(self):
-        outputtxt = io.StringIO()   
-        iotext = io.StringIO("10")
-        calc = Calc_Price(iotext,outputtxt)
+
+
+        self.lineText("""10,12
+40,16
+100,45
+
+50,50,55""","""24
+62
+160
+0
+171
+""")
+
+    def lineText(self,txt,expecttxt):
+        outputtxt = io.StringIO()
+        iotext = io.StringIO(txt)
+        calc = Calc_Price(iotext, outputtxt)
         calc.calc_price()
-        self.assertEqual(calc.outputtxt.getvalue(),"11\n")
+        self.assertEqual(calc.outputtxt.getvalue(), expecttxt)
