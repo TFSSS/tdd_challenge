94eda13b7ac05e7a4e8f92677f43bef240907ffe
shungokusatsu <fukand-takuya-bw@ynu.jp>
2018-09-07 16:59:22 +0900

    calc_price実装開始

--- calc_price.py
+++ calc_price.py
@@ -1,6 +1,19 @@
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 
-class CalcPrice:
-    def calc_price(self,priceList):
-        priceSumCt = sum(priceList) * 1.1
-        return Decimal(str(priceSumCt)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
+class Calc_Price:
+    def __init__(self,inputtxt):
+        self._inputtxt = inputtxt
+
+    def calc_price(self):
+        for line in self._inputtxt.readlines():
+            prices = line.split(",")
+            priceSumCt = sum(prices) * 1.1
+            priceSumCt = Decimal(str(priceSumCt)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
+            print(priceSumCt)
+
+if __name__ == '__main__':
+    # 本番はこんな感じで使うことを想定
+    kvs = SomeKVSUsingDynamoDB()
+    my_memory = MyMemory(kvs)
+    my_memory.set_favorite('ボルダリング')
+    print(my_memory.get_favorite())
--- test/calc_price_test.py
+++ test/calc_price_test.py
@@ -1,9 +1,11 @@
 import unittest
-
-from calc_price import CalcPrice
+from calc_price import Calc_Price
 import sys
 import io
 
-
 class CalcPriceTest(unittest.TestCase):
     def testCreate(self):
+        iotext = io.StringIO("10")
+        sys.stdout = iotext
+        calc = Calc_Price(iotext)
+        self.assertEqual(iotext.getvalue(),"11")
